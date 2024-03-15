ef846c3c4ac5b5d9ccd34a4e15b15720dc26c1aa
Ajay <chavdaajay129@gmail.com>
2024-03-15 15:19:50 +0530

    chnage footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = ()=>{
     return(
         <div className="footer">
-            <a href="https://instagram.com/_ajay.026">&copy; Created By Ajay</a>
+            <p>&copy; Created By Nick</p>
         </div>
     )
 }
