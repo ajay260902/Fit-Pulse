56306be0043117ddc529551ba6c88b8860b05d12
Chavda Ajay <111041866+ajay260902@users.noreply.github.com>
2024-10-29 17:28:11 +0530

    adding if token expire then logout

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -19,12 +19,22 @@ export const AuthContextProvider = ({ children }) => {
   })
 
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'))
-
+    const user = JSON.parse(localStorage.getItem('user'));
+    
     if (user) {
-      dispatch({ type: 'LOGIN', payload: user }) 
+      const decodedToken = jwt_decode(user.token); // Decode the JWT
+      const currentTime = Date.now() / 1000; // Current time in seconds
+
+      if (decodedToken.exp < currentTime) {
+        // Token is expired
+        dispatch({ type: 'LOGOUT' });
+        localStorage.removeItem('user'); // Remove user from local storage
+      } else {
+        // Token is valid
+        dispatch({ type: 'LOGIN', payload: user });
+      }
     }
-  }, [])
+  }, []);
 
   console.log('AuthContext state:', state)
   
