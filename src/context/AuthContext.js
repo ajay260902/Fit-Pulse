81670ac11b02f3dc3b227227873c235c3557189c
ajay.chavda <ajay.chavda@safeinvest.co.in>
2024-10-30 13:30:43 +0530

    quick fix

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useReducer, useEffect } from 'react'
-import {jwtDecode} from 'jwt-decode';
+
 
 
 export const AuthContext = createContext()
@@ -24,17 +24,17 @@ export const AuthContextProvider = ({ children }) => {
     const user = JSON.parse(localStorage.getItem('user'));
     
     if (user) {
-      const decodedToken = jwtDecode(user.token); // Decode the JWT
-      const currentTime = Date.now() / 1000; // Current time in seconds
+      // const decodedToken = jwtDecode(user.token); // Decode the JWT
+      // const currentTime = Date.now() / 1000; // Current time in seconds
 
-      if (decodedToken.exp < currentTime) {
-        // Token is expired
-        dispatch({ type: 'LOGOUT' });
-        localStorage.removeItem('user'); // Remove user from local storage
-      } else {
+      // if (decodedToken.exp < currentTime) {
+      //   // Token is expired
+      //   dispatch({ type: 'LOGOUT' });
+      //   localStorage.removeItem('user'); // Remove user from local storage
+      // } else {
         // Token is valid
         dispatch({ type: 'LOGIN', payload: user });
-      }
+      // }
     }
   }, []);
 
